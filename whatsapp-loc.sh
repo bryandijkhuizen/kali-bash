5102acd56bb11a21cf4aadb84dd75004f7191199
Bryan Dijkhuizen <dijkhuizenb@outlook.com>
2021-02-07 23:27:29 +0100

    Update whatsapp-loc.sh

--- whatsapp-loc.sh
+++ whatsapp-loc.sh
@@ -4,11 +4,11 @@ cd seeker && ls
 
 sudo bash install.sh
 
-cd Pictures
+cd ../Pictures
 
 curl "https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1327&q=80 > image.png"
 
-cd ..
+cd kali-bash
 
 python3 seeker.py -t manual
 
